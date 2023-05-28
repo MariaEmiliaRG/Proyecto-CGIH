9c7662635a88101e0640de5a004ea0c5372766df
María Emilia Ramírez <mariaemiliaramirezgomez@comunidad.unam.mx>
2023-05-28 00:38:39 -0600

    feat: se agregaron rocas y plantas

--- lcgihc_diorama/lcgihc_diorama/lcgihc_diorama.cpp
+++ lcgihc_diorama/lcgihc_diorama/lcgihc_diorama.cpp
@@ -515,7 +515,7 @@ int main()
 		
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(-35.0f, 5.0f, -28.0f));
-		model = glm::scale(model, glm::vec3(0.5f, 0.5f, 0.5f));
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		brujula.RenderModel();
@@ -533,17 +533,47 @@ int main()
 
 		float rocas[] = {
 			10.0f, 2.0f, -56.0f, 5.0f, 1.0f, 5.0f,
-			15.0f, 2.0f, -49.0f, 3.0f, 1.0f, 5.0f, 
-			-38.0f, 2.0f, -16.0f, 3.0f, 2.0f, 4.0f, 
+			15.0f, 2.0f, -49.0f, 3.0f, 1.0f, 5.0f,
+			-38.0f, 2.0f, -16.0f, 3.0f, 2.0f, 4.0f,
 			-25.0f, 3.0f, -20.0f, 4.0f, 1.0f, 3.0f,
 			-30.0f, 2.0f, -18.0f, 5.0f, 2.0f, 1.5f,
 			-48.0f, 2.0f, 35.0f, 4.0f, 2.0f, 2.0f,
 			-36.0f, 2.0f, 42.0f, 3.0f, 2.0f, 5.0f,
+			20.0f, 2.0f, 65.0f, 6.0f, 3.0f, 3.0f,
+			25.0f, 2.0f, 50.0f, 3.0f, 1.0f, 2.0f,
+			23.0f, 2.0f, 55.0f, 3.0f, 1.0f, 5.0f,
+			85.0f, 2.0f, 35.0f, 5.0f, 3.5f, 1.0f,
+			64.0f, 2.0f, 15.0f, 6.0f, 3.0f, 2.5f,
+			95.0f, 2.0f, 26.0f, 4.5f, 2.0f, 8.0f, 
+			72.0f, 2.0f, 45.0f, 5.0f, 3.5f, 4.0f,
+			34.0f, 2.0f, 65.0f, 3.0f, 3.0f, 6.5f,
+			86.0f, 2.0f, 32.0f, 6.5f, 2.0f, 7.0f,
+			20.0f, 2.0f, -65.0f, 6.0f, 3.0f, 3.0f,
+			25.0f, 2.0f, -50.0f, 2.0f, 1.0f, 2.0f,
+			23.0f, 2.0f, -55.0f, 3.0f, 1.0f, 5.0f,
+			85.0f, 2.0f, -35.0f, 4.0f, 3.5f, 9.0f,
+			64.0f, 2.0f, -15.0f, 6.0f, 3.0f, 2.5f,
+			95.0f, 2.0f, -26.0f, 4.5f, 2.0f, 4.0f,
+			72.0f, 2.0f, -45.0f, 1.0f, 3.5f, 5.0f,
+			34.0f, 2.0f, -65.0f, 3.0f, 3.0f, 6.5f,
+			86.0f, 2.0f, -32.0f, 7.5f, 2.0f, 4.0f,
+			-34.0f, 2.0f, -65.0f, 3.0f, 1.0f, 6.5f,
+			-86.0f, 2.0f, -32.0f, 4.5f, 2.0f, 6.0f,
+			-20.0f, 2.0f, -65.0f, 1.0f, 1.0f, 3.0f,
+			-25.0f, 2.0f, -50.0f, 2.0f, 1.0f, 2.0f,
+			-23.0f, 2.0f, -55.0f, 3.0f, 1.0f, 5.0f,
+			-85.0f, 2.0f, -35.0f, 4.0f, 3.5f, 8.0f,
+			-64.0f, 2.0f, -15.0f, 5.0f, 2.0f, 2.5f,
+			-95.0f, 2.0f, -26.0f, 4.5f, 2.0f, 4.0f,
+			-72.0f, 2.0f, -45.0f, 1.0f, 1.5f, 5.0f,
+			-34.0f, 2.0f, -65.0f, 3.0f, 3.0f, 3.5f,
+			-86.0f, 2.0f, -32.0f, 2.5f, 2.0f, 5.0f,
+
 		};
 
 		int j;
 
-		for (i = 0; i < 7; i++) {
+		for (i = 0; i < (sizeof(rocas)/sizeof(rocas[0]))/6 ; i++) {
 
 			j = 6 * i;
 			model = glm::mat4(1.0);
@@ -555,6 +585,30 @@ int main()
 			roca.RenderModel();
 		}
 
+		float plantas[] = {
+			12.0f, 1.0f, -51.0f, 1.0f, 2.0f, 1.0f,
+			10.0f, 2.0f, -40.0f, 0.5f, 0.5f, 0.5f,
+			-38.0f, 2.0f, -16.0f, 1.0f, 2.0f, 1.0f,
+			-27.0f, 3.0f, -23.0f, 1.5f, 1.0f, 1.0f,
+			-30.0f, 2.0f, -18.0f, 1.0f, 2.0f, 1.0f,
+			-45.0f, 2.0f, 27.0f, 1.0f, 0.5f, 1.0f,
+			-30.0f, 2.0f, 32.0f, 3.0f, 2.0f, 1.0f,
+			14.0f, 2.0f, 45.0f, 1.0f, 1.0f, 1.0f,
+			30.0f, 2.0f, 36.0f, 1.0f, 1.5f, 1.0f,
+			32.0f, 2.0f, 25.0f, 0.5f, 0.5f, 0.5f
+		};
+
+		for (i = 0; i < (sizeof(plantas) / sizeof(plantas[0])) / 6; i++) {
+
+			j = 6 * i;
+			model = glm::mat4(1.0);
+			model = glm::translate(model, glm::vec3(plantas[j], plantas[j + 1], plantas[j + 2]));
+			model = glm::scale(model, glm::vec3(plantas[j + 3], plantas[j + 4], plantas[j + 5]));
+			glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+			Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
+			planta.RenderModel();
+		}
+
 		model = glm::mat4(1.0);
 		model = glm::translate(model, glm::vec3(-24.0f, 4.0f, 29.0f));
 		model = glm::scale(model, glm::vec3(2.5f, 2.5f, 2.5f)); 
@@ -564,37 +618,12 @@ int main()
 		neritantan.RenderModel();
 
 
-		/*
 		model = glm::mat4(1.0);
-		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+		model = glm::translate(model, glm::vec3(62.0f, 13.0f, 10.0f));
 		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-		//model = glm::rotate(model, -90 * toRadians, glm::vec3(1.0f, 0.0f, 0.0f)); 
-		//model = glm::rotate(model, -90 * toRadians, glm::vec3(0.0f, 0.0f, 1.0f)); 
-		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
-		neritantan.RenderModel();
-
-		model = glm::mat4(1.0);
-		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-		//model = glm::rotate(model, -90 * toRadians, glm::vec3(1.0f, 0.0f, 0.0f)); 
-		//model = glm::rotate(model, -90 * toRadians, glm::vec3(0.0f, 0.0f, 1.0f)); 
-		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
-		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
-		planta.RenderModel();
-
-		model = glm::mat4(1.0);
-		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
-		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
-		//model = glm::rotate(model, -90 * toRadians, glm::vec3(1.0f, 0.0f, 0.0f)); 
-		//model = glm::rotate(model, -90 * toRadians, glm::vec3(0.0f, 0.0f, 1.0f)); 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		reliquia.RenderModel();
-
-		
-
-		*/
 
 		//AVATARS
 		
